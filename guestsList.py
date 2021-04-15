1cdea0298ef12118652855ad9d7ab5e31b5a074a
Ni4ka <astahova_nica@mail.ru>
2021-04-15 11:59:18 +0300

    Add file guestsList.py

--- guestsList.py
+++ guestsList.py
@@ -5,21 +5,49 @@ from os import system
 
 # ### DATA #################
 
-guests_1 = [ "Bethaney Bain", "Kacey Johns", "Manpreet Saunders" ]
-guests_2 = [ "Elwood Curtis", "Diya Griffin", "Kacey Johns" ]
-guests_3 = [ "Tobey Weiss", "Kadie Barnes", "Diya Griffin" ]
+guests_1 = [
+ "Bethaney Bain",
+ "Kacey Johns",
+ "Manpreet Saunders" 
+ ] 
+
+guests_2 = [
+ "Elwood Curtis",
+ "Diya Griffin",
+ "Kacey Johns"
+ ]
+
+guests_3 = [
+ "Tobey Weiss",
+ "Kadie Barnes",
+ "Diya Griffin" 
+ ]
 
 
 # ### DATA ################
 
+# ### LOGIC ##############
 
-system( "clear" )
+finalList = []
 
 guests_1 += guests_2
 guests_1 += guests_3
 
-print( guests_1 )
+for i in guests_1:
+ if i not in finalList:
+  finalList.append( i )
+
+finalList.sort()
+
+# ### LOGIC ############
 
 
-#guests_1.extend( guests_2 )
-#print( guests_1 )
+# ### VIEW #############
+n = 1
+system( "clear" )
+print( "GUESTS LIST:\n" ) 
+for i in finalList:
+ print( f"{ n } > { i }" )
+ n += 1
+print()
+# ### VIEW #############
